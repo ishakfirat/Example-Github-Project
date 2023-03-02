896be0b473dc75498b08815384c08066202b4f7b
Ishak Firat <iishakfirat@gmail.com>
2023-03-02 22:18:18 +0000

    Created class5Topics class

---
+++ src/test/java/unit1/Claas5Topics.java
@@ -0,0 +1,19 @@
+package unit1;
+
+public class Claas5Topics {
+
+
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 8;
+
+        //Add the numbers a and b
+        int total= a+b;
+        System.out.println(total);
+
+
+    }
+
+
+}
